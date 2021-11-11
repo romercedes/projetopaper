7455713d9f2f0188b89a3d02663923da5fdab5fb
romercedes <ro.santo.mercedes@gmail.com>
2021-11-11 11:51:14 -0300

    commit 2

---
+++ paper/questoes/models.py
@@ -0,0 +1,23 @@
+import datetime
+
+from django.db import models
+from django.utils import timezone
+
+class Question(models.Model):
+    question_text = models.CharField(max_length=200)
+    habilidade = models.CharField(max_length=100)
+    pub_date = models.DateTimeField('data de publicação')
+    def __str__(self):
+        return self.question_text
+    def was_published_recently(self):
+        return self.pub_date >= timezone.now() - datetime.timedelta(days=1)
+
+
+class Choice(models.Model):
+    question = models.ForeignKey(Question, on_delete=models.CASCADE)
+    choice_text = models.CharField(max_length=200)
+    escolha = models.IntegerField(default=0)
+    def __str__(self):
+        return self.choice_text
+
+# Create your models here.
