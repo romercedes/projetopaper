86f8bb02f26d50c0d23afc7097207c931c48186f
romercedes <ro.santo.mercedes@gmail.com>
2021-11-14 12:44:12 -0300

    commit 5

--- paper/paper/urls.py
+++ paper/paper/urls.py
@@ -17,6 +17,6 @@ from django.contrib import admin
 from django.urls import include, path
 
 urlpatterns = [
-    path('questoes', include('questoes.urls')),
+    path('', include('questoes.urls')),
     path('admin/', admin.site.urls),
 ]
