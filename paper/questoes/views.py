4f26e4a4ea61cacb43d10add6fa1ce986b650b7c
romercedes <ro.santo.mercedes@gmail.com>
2021-11-21 22:02:51 -0300

    commit 7

--- paper/questoes/views.py
+++ paper/questoes/views.py
@@ -20,8 +20,11 @@ def resposta(request, question_id):
     return render(request, 'questoes/resultados.html', {'question': question})
 
 def index(request):
-    latest_question_list = Question.objects.order_by('-pub_date')[:5]
-    context = {'latest_question_list': latest_question_list}
+    latest_question_list = Question.objects.filter(habilidade='Soma')[:10]
+    context = {
+        'latest_question_list': latest_question_list
+        'habilidades': Question.objects.all()
+        }
     return render(request, 'questoes/index.html', context)
 
 def detail(request, question_id):
